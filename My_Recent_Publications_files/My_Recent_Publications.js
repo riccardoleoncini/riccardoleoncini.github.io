81f7266d67b3d05d24ca470a8d8bdd0b95d3cf04
riccardoleoncini <riccardo.leoncini@unibo.it>
2023-03-21 22:16:42 +0100

    PErsonal webpage

---
+++ My_Recent_Publications_files/My_Recent_Publications.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.4 local-build-20170822
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('My_Recent_Publications_files/My_Recent_PublicationsMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
