ac183bb0af7da71e3b6cf18f459f25ac1d5bc0bd
riccardoleoncini <riccardo.leoncini@unibo.it>
2026-02-11 23:04:55 +0100

    RL

---
+++ images/CV.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.4 local-build-20191002
+
+setTransparentGifURL('Media/transparent.gif');function applyEffects()
+{var registry=IWCreateEffectRegistry();registry.registerEffects({shadow_0:new IWShadow({blurRadius:1,offset:new IWPoint(-0.0000,1.0000),color:'#000000',opacity:0.200000}),stroke_0:new IWPhotoFrame([IWCreateImage('CV_files/Formal_inset_01.png'),IWCreateImage('CV_files/Formal_inset_02.png'),IWCreateImage('CV_files/Formal_inset_03.png'),IWCreateImage('CV_files/Formal_inset_06.png'),IWCreateImage('CV_files/Formal_inset_09.png'),IWCreateImage('CV_files/Formal_inset_08.png'),IWCreateImage('CV_files/Formal_inset_07.png'),IWCreateImage('CV_files/Formal_inset_04.png')],null,0,0.800000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000),stroke_1:new IWPhotoFrame([IWCreateImage('CV_files/Formal_inset_01_1.png'),IWCreateImage('CV_files/Formal_inset_02_1.png'),IWCreateImage('CV_files/Formal_inset_03_1.png'),IWCreateImage('CV_files/Formal_inset_06_1.png'),IWCreateImage('CV_files/Formal_inset_09_1.png'),IWCreateImage('CV_files/Formal_inset_08_1.png'),IWCreateImage('CV_files/Formal_inset_07_1.png'),IWCreateImage('CV_files/Formal_inset_04_1.png')],null,0,0.800000,1.000000,1.000000,1.000000,1.000000,14.000000,14.000000,14.000000,14.000000,191.000000,262.000000,191.000000,262.000000,null,null,null,0.100000)});registry.applyEffects();}
+function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('CV_files/CVMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');applyEffects()}
+function onPageUnload()
+{Widget.onunload();}
